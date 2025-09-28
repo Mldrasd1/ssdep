bc29313b0805cb5e5e90b3353867b90e0910a95e
Mldrasd1 <156513762+Mldrasd1@users.noreply.github.com>
2025-09-28 14:08:57 +0100

    finishing the html for the website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./components/header.css"
+import "./components/orders.css"
+import "./components/tracking.css"
+import "./components/homepage.css"
+import "./components/checkout.css"
+import "./components/checkout-header.css"
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +29,32 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <div className="header">
+      <div className="left-section">
+        <Link href="/" className="logo-link">MOULOUD</Link>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+
+        <button className="search-button">
+          <img className="search-icon" src="images/icons/search-icon.png" />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <a className="orders-link header-link" href="/orders">
+
+          <span className="orders-text">Orders</span>
+        </a>
+
+        <a className="cart-link header-link" href="checkout">
+          <img className="cart-icon" src="images/icons/cart-icon.png" />
+          <div className="cart-quantity">3</div>
+          <div className="cart-text">Cart</div>
+        </a>
+      </div>
+    </div>
         {children}
       </body>
     </html>
