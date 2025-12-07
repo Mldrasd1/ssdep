1164f117a1aa93cd166803a1a5456f1821315e65
mouloud2003 <mouloud.mohamed-ali@etu.ensti-annaba.dz>
2025-12-07 08:53:09 +0100

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,19 @@
-'use client'
+"use client";
 import Image from "next/image";
 import axios from "axios";
-import { useEffect ,  useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./components/Product";
-
-
+//testig
 export default function Home() {
   const [product, setProduct] = useState([]);
   useEffect(() => {
-    axios.get('https://backendmodules.onrender.com/api/products')
-  .then((response) => 
-    { setProduct(response.data); // here with axios we dont want to use json() we get the data from the response directly
-    })
+    axios
+      .get("https://backendmodules.onrender.com/api/products")
+      .then((response) => {
+        setProduct(response.data); // here with axios we dont want to use json() we get the data from the response directly
+      });
   }, []);
-  
- 
+
   /* the same thing as axios but this is harder to handle
   fetch('https://backendmodules.onrender.com/api/products')
   .then((response) => 
@@ -24,18 +23,14 @@ return response.json() we use json to convert the response to json format
        console.log(data); // we get the data from the response
 })*/
   return (
-   <>
-    <div className="home-page">
-      <div className="products-grid">
-          { product.map((product) => ( 
-             
+    <>
+      <div className="home-page">
+        <div className="products-grid">
+          {product.map((product) => (
             <Product key={product.id} product={product} />
-            
-          ) )}
-          
+          ))}
         </div>
       </div>
-
-   </>
+    </>
   );
 }
